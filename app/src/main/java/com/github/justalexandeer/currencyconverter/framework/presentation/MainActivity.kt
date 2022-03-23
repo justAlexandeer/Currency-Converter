68e55d3bb324c3911ef1c948dcbc55d3ba7a37ff
Alexander Semenyuk <facemmannn403@gmail.com>
2022-03-23 17:18:18 +0300

    init project

---
+++ app/src/main/java/com/github/justalexandeer/currencyconverter/framework/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.github.justalexandeer.currencyconverter.framework.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.justalexandeer.currencyconverter.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
